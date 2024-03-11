46d47c7e49089bca656e6bb43e72362e76069113
AniutaP <musenok.ko@mail.ru>
2024-03-11 15:54:28 +0300

    update

--- hockey_django_project/settings.py
+++ hockey_django_project/settings.py
@@ -150,7 +150,7 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'hockey_django_project/static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
